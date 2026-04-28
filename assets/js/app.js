b2c309e351553752d2ffe940a4818341db92ddc1
KotaMatsumoto0206 <2ghmk6@gmail.com>
2026-04-28 20:54:49 +0900

    Feat: jsにてDom取得

---
+++ assets/js/app.js
@@ -0,0 +1,8 @@
+"use strict";
+
+// DOMの取得
+const todoInput = document.querySelector('#todo-input');
+const addButton = document.querySelector('.add-button');
+const todoList = document.querySelector('.todo-list');
+
+// 
